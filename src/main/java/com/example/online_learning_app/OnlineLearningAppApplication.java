048c4b8d3e82684f376a6dda11df07662f49c770
HusanboyJorayev <144492116+HusanboyJorayev@users.noreply.github.com>
2024-03-15 00:00:21 +0500

    Initial commit

---
+++ src/main/java/com/example/online_learning_app/OnlineLearningAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.online_learning_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineLearningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineLearningAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/online_learning_app/OnlineLearningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.online_learning_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLearningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
